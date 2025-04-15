f5b6e32cfddcf164ae885adfc9aa27b2b77a091e
Marshall Baldwin <marshall.baldwin@ai-nv-a01.wx.noaa.gov>
2025-04-15 22:40:24 +0000

    add basic unet

--- models/vanilla_unet.py
+++ models/vanilla_unet.py
@@ -31,52 +31,46 @@ class LayerNorm(nn.Module):
             x = self.weight[:, None, None] * x + self.bias[:, None, None]
             return x
 
-class FilmedConvBlock(nn.Module):
-    """
-    ConvBlock which includes Feature-Wise Linear Modulation (FiLM; Perez 2017) for a time/sigma embedding
-    """
-    def __init__(self, channels_in=1, channels_out=1, kernel_size=3, condition_dim=32):
+class ConvBlock(nn.Module):
+    def __init__(self, channels_in = 1, channels_out = 1, kernel_size=3):
         super().__init__()
-        self.conv = nn.Conv2d(channels_in, channels_out, kernel_size, padding=kernel_size // 2)
+        self.conv = nn.Conv2d(channels_in, channels_out, kernel_size, padding=kernel_size//2)
         self.norm = LayerNorm(channels_out, eps=1e-6)
         self.act = nn.ReLU()
-        self.film = nn.Linear(condition_dim, channels_out * 2) #the *2 is for scaling and shifting 
 
-    def forward(self, x, cond_embedding):
+    def forward(self, x):
+        """
+        Convolves x with the block's kernel, normalizes, then passes through activation functon
+        """
 
-        #do convolutions and normalization as usual
         x = self.conv(x)
-        x = x.permute(0, 2, 3, 1)
-        x = self.norm(x)
-        x = x.permute(0, 3, 1, 2)  # (B, H, W, C) -> (B, C, H, W)
-        
-        #adjust behavior with FiLM; see Section 2.1 in Perez 2017
-        film_params = self.film(cond_embedding)  # model f and h with a single linear layer -> dim [B, 2*C]
-        scale, shift = film_params.chunk(2, dim=1) #splits into two tensors of dim [B, C]
-        scale = scale.view(x.shape[0], x.shape[1], 1, 1)
-        shift = shift.view(x.shape[0], x.shape[1], 1, 1)
-        x = x * (1 + scale) + shift  #apply the learned affine transformation
-
-        #continue business as usual
-        x = self.act(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C) to allow norm to work
+        x = self.norm(x) #TODO: Try BatchNorm
+        x = self.act(x) #TODO: Use LeakyReLU with slope param [.01, .2]
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
         return x
 
-class DenoisingUNet(nn.Module):
+#little helper class to enable concise conv block stacks
+class CustomSequential(nn.Sequential):
+    def forward(self, input, *args, **kwargs):
+        for module in self:
+            input = module(input, *args, **kwargs)
+        return input
+        
+class VanillaUNet(nn.Module):
     def __init__(self,
                  channels_in=1,
                  channels_out=1,
                  n_conv_filters=[16, 16, 16], # last element indicates # of bottleneck conv filters
                  n_layers_per_block=1, #number of convolutional blocks prior to up/downsampling
-                 n_bottleneck_blocks=1, #number of convolutional blocks at smallest "bottleneck" resolution
-                 cond_embedding_dim=16 #length of timestep embedding vector; should be even! (sinusoidal embedding)
+                 n_bottleneck_blocks=1 #number of convolutional blocks at smallest "bottleneck" resolution
                  ):
         
         super().__init__()
 
         self.channels_in = channels_in
         self.channels_out = channels_out
-        self.cond_embedding_dim = cond_embedding_dim
-        self.n_encoder_layers = len(n_conv_filters) - 1
+        self.n_encoder_layers = len(n_conv_filters) - 1 #last one is for bottleneck
 
         self.pool = nn.AvgPool2d((2, 2), stride=2)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
@@ -85,10 +79,9 @@ class DenoisingUNet(nn.Module):
         #stack of encoder layers with multiple FilmedConvBlocks per layer
         self.encoder_layers = nn.ModuleList([
             CustomSequential(*[
-                FilmedConvBlock(channels_in=(self.channels_in if i == 0 and j == 0 else n_conv_filters[i - (1 if j == 0 else 0)]),
+                ConvBlock(channels_in=(self.channels_in if i == 0 and j == 0 else n_conv_filters[i - (1 if j == 0 else 0)]),
                               channels_out=n_conv_filters[i],
-                              kernel_size=3,
-                              condition_dim=self.cond_embedding_dim)
+                              kernel_size=3)
                 for j in range(n_layers_per_block)
             ])
             for i in range(self.n_encoder_layers)
@@ -96,20 +89,18 @@ class DenoisingUNet(nn.Module):
 
         #stack of bottleneck Layers with multiple FilmedConvBlocks per layer
         self.bottleneck_layers = CustomSequential(*[
-            FilmedConvBlock(channels_in=n_conv_filters[self.n_encoder_layers - 1] if i == 0 else n_conv_filters[-1], 
+            ConvBlock(channels_in=n_conv_filters[self.n_encoder_layers - 1] if i == 0 else n_conv_filters[-1], 
                         channels_out=n_conv_filters[-1],
-                        kernel_size=3,
-                        condition_dim=self.cond_embedding_dim)
+                        kernel_size=3)
             for i in range(n_bottleneck_blocks)
         ])
 
         #stack of decoder layers with multiple FilmedConvBlocks per layer
         self.decoder_layers = nn.ModuleList([
             CustomSequential(*[
-                FilmedConvBlock(channels_in=(n_conv_filters[i] + n_conv_filters[i - 1]) if j == 0 else n_conv_filters[i - 1],
+                ConvBlock(channels_in=(n_conv_filters[i] + n_conv_filters[i - 1]) if j == 0 else n_conv_filters[i - 1],
                               channels_out=n_conv_filters[i - 1],
-                              kernel_size=3,
-                              condition_dim=self.cond_embedding_dim)
+                              kernel_size=3)
                 for j in range(n_layers_per_block)
             ])
             for i in range(self.n_encoder_layers, 0, -1)
@@ -117,30 +108,23 @@ class DenoisingUNet(nn.Module):
         
         
 
-    def forward(self, x, t):
-        """
-        x is the noisy channels + conditioning channels. t is the "timestep" in noise space.
-        Note that when wrapped via EDMPrecond, t is automatically casted to a torch.Tensor (what is passed here).
-        """
-        
-        #transform noise timesteps to embedding vectors
-        t_emb = self.get_timestep_embedding(t, self.cond_embedding_dim, flip_sin_to_cos = True)
+    def forward(self, x):
 
         #loop through each encoder layer, saving output for decoding path
         enc_outputs = []
         for i,enc_layer in enumerate(self.encoder_layers):
-            x = enc_layer(x, t_emb)
+            x = enc_layer(x)
             enc_outputs.append(x)
             x = self.pool(x)
 
         #do bottleneck convolutions
-        x = self.bottleneck_layers(x, t_emb)
+        x = self.bottleneck_layers(x)
  
         #loop through each decoder layer, concatenating encoder layer output as extra channels
         for i, dec_layer in enumerate(self.decoder_layers):
             x = self.upsample(x)
             x = torch.cat([x, enc_outputs[-(i + 1)]], dim=1)
-            x = dec_layer(x, t_emb)
+            x = dec_layer(x)
 
         #pass through a final linear layer 
         x = self.final(x)
